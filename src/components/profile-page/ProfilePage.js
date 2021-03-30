172dcce469c7108506105e6ec7871d998c72051b
Weston Woodard <westonwoodard28@gmail.com>
2021-03-30 15:52:22 -0700

    rough draft commplete.  waiting for backend

--- src/components/profile-page/ProfilePage.js
+++ src/components/profile-page/ProfilePage.js
@@ -40,24 +40,19 @@ const Profile = (props) => {
     return (
         <Page>
             <Header>
+                <p></p>
                 <HeaderTitle>Welcome back, {user.username}</HeaderTitle>
                 <HeaderButton onClick={handleClick}>logout</HeaderButton>
             </Header>
             <UserCard>
-                <UserInfo>
-                    <Username>{user.username}</Username>
-                    <Email>{user.email}</Email>
-                </UserInfo>
-                <UserPicture>
-                    {/*placeholder image */}
-                    <UserPic src="https://img.huffingtonpost.com/asset/5967ac4d2100003700fc6bc9.jpeg" alt="Person Cooking" />
-                </UserPicture>
+                <UserPic src="https://img.huffingtonpost.com/asset/5967ac4d2100003700fc6bc9.jpeg" alt="Person Cooking" />
             </UserCard>
             <RecipeSearch>
+                <p>Search for Recipe Here:</p>
                 <input
                     type='text'
                     name='recipe'
-                    placeholder='search for recipe name here'
+                    placeholder='Recipe Name'
                     value={search}
                     onChange={handleChange}
                 />
@@ -91,9 +86,12 @@ const Page = styled.div`
     font-family: 'Roboto', sans-serif;
     display: flex;
     flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
 `
 
     const Header = styled.header`
+        width: 100%;
         height: 10vh;
         display: flex;
         flex-direction: row;
@@ -102,40 +100,60 @@ const Page = styled.div`
     `
 
 
-        const HeaderTitle = styled.h1``
+        const HeaderTitle = styled.h1`
+            color: #813D18;
+            font-size: 40px;
+            margin-left: 105px;
+        `
 
         const HeaderButton = styled.button `
             height: 50%;
+            width: 100px;
+            box-shadow: 3px 3px #4D6E7F;
+            margin-right: 5px;   
         `
 
     const UserCard = styled.section`
         height: 20vh;
+        width: 50%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-bottom: 3px;
     `
 
-        const UserInfo = styled.div``
-        
-            const Username = styled.h2``
-
-            const Email = styled.p``
-
-        const UserPicture = styled.div`
-            
-        `
-
             const UserPic = styled.img`
-                width: 25%;
+                height: 100%;
+                box-shadow: 3px 3px #4D6E7F;
             `
 
     const RecipeSearch = styled.form`
          height: 10vh;
+         display: flex;
+         flex-direction: column;
+         justify-content: flex-end;
+         margin-bottom: 5px;
+
     `
 
     const RecipeCards = styled.section`
          height: 50vh;
+         border: 2px solid black;
+         display: flex;
+         flex-wrap: wrap;
+         flex-direction: row;
+         justify-content: center;
+         align-items: center;
     `
 
     const Footer = styled.footer`
         height: 10vh;
+        display: flex;
+        justify-content: center;
+        align-items: flex-end;
+
     `
 
-        const FooterButton = styled.button``
+        const FooterButton = styled.button`
+        height: 50%;
+        `
--- src/components/profile-page/RecipeComponent.js
+++ src/components/profile-page/RecipeComponent.js
@@ -15,15 +15,15 @@ const Recipe = ({recipe, search}) => {
                 <RecipePic src='https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg' alt='delicious food'/>
             </RecipeHeader>
             <RecipeDescription>
-                <p>{recipe.category}</p>
-                <p>{recipe.timeToMake}</p>
-                <p>{recipe.description}</p>
+                <p>{`Style: ${recipe.category}`}</p>
+                <p>{`Time Required to Make: ${recipe.timeToMake}`}</p>
+                <p>{`Description: ${recipe.description}`}</p>
             </RecipeDescription>
             <RecipeIngredients>
-                <p>{recipe.ingredients}</p>
+                <p>{`Ingredients: ${recipe.ingredients}`}</p>
             </RecipeIngredients>
             <RecipeInstructions>
-                <p>{recipe.instructions}</p>
+                <p>{`Instructions: ${recipe.instructions}`}</p>
             </RecipeInstructions>
         </RecipeComponent>
     )
@@ -35,27 +35,42 @@ export default Recipe;
 //styling below this line
 
 const RecipeComponent = styled.div`
-    height: fit-content;
+    width: 20%;
+    border: 2px solid #813D18;
+    box-shadow: 3px 3px #4D6E7F;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
 `
 
     const RecipeHeader = styled.div`
         height: 10%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-bottom: 20px;
         `
 
-        const RecipeTitle = styled.h3``
+        const RecipeTitle = styled.h3`
+        `
 
         const RecipePic = styled.img`
-            width: 25%;;
+            width: 25%;
         `
 
     const RecipeDescription = styled.div`
         height: 20%;
+        margin-bottom: 20px;
     `
 
     const RecipeIngredients = styled.div`
         height: 20%;
+        margin-bottom: 20px;
     `
 
     const RecipeInstructions = styled.div`
         height: 20%;
+        margin-bottom: 20px;
     `
