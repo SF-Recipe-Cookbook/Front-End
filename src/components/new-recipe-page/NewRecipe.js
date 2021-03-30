ddd2340221db7f60eafa6eaf5843db8059c47030
bukit3point0 <bukit3point0@gmail.com>
2021-03-30 14:22:22 -0400

    styling complete, working on array creation

--- src/components/new-recipe-page/NewRecipe.js
+++ src/components/new-recipe-page/NewRecipe.js
@@ -3,11 +3,17 @@ import styled from 'styled-components'
 
 // Enter New Recipe Component - form must include recipe title, category, description, instructions, ingredients, and time required to make
 
-const Page = styled.div``
+const Page = styled.div`
+    font-size: 1.5rem;
+    color: #813D18;
+    width: 40%;
+    margin: auto;
+`
 const RecipeName = styled.h1`
     font-size: 3rem;
     text-align: center;
-    margin: 1rem;
+    margin: 1rem 0;
+    font-weight: bold;
 `
 const NewRecipeCard = styled.form`
     display: flex;
@@ -15,40 +21,82 @@ const NewRecipeCard = styled.form`
     align-items: center;
     justify-content: center;
 `
+const FirstRow = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+`
 const Text = styled.p`
-    margin: .5rem;
+    font-size: 1.2rem;
+    margin: .15rem .5rem;
 `
 const Title = styled.input`
-    margin: .5rem;
-    width: 30rem;
+    margin: .5rem 1rem;
+    font-size: 2rem;
+    /* width: 20rem; */
+    color: #813D18;
+    background-color: inherit;
+    border: none;
+    border-bottom: 3px solid #333D45;
+
+    &::-webkit-input-placeholder {
+        color: #D5C9BB;
+    }
 `
 const Category = styled.select`
-    margin: .5rem;
-    width: 30rem;
+    margin: .5rem 1rem;
+    /* width: 8rem; */
+    background-color: inherit;
+    border: none;
+    /* font-size: 1rem; */
 `
-const Description = styled.input`
+const Description = styled.textarea`
     margin: .5rem;
-    width: 30rem;
+    width: 80%;
 `
-const Ingredients = styled.input`
+const BottomCont = styled.div`
+    display: flex;
+    width: 100%;
+    height: 30vh;
+    justify-content: center;
+`
+const Ingredients = styled.textarea`
     margin: .5rem;
-    width: 30rem;
+    width: 30%;
+`
+const RightCont = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 50%;
 `
 const Instructions = styled.textarea`
-    width: 20%;
+    width: 95%;
     margin: .5rem;
-    width: 30rem;
+    height: 25vh;
 `
-const TimePrep = styled.input`
-    margin: .5rem;
-    width: 30rem;
+const TimerCont = styled.div`
+    display: flex;
+    font-size: 1.2rem;
+    justify-content: space-between;
 `
-const TimeCook = styled.input`
-    margin: .5rem;
-    width: 30rem;
+const Timer = styled.input`
+    margin: 0 .5rem;
+    width: 4rem;
+    color: #813D18;
+    background-color: inherit;
+    border: none;
+    border-bottom: 2px solid #333D45;
+
+    &::-webkit-input-placeholder {
+        color: #D5C9BB;
+    }
 `
 const AddRecipe = styled.button`
     margin: .5rem;
+    background-color: inherit;
+    border: none;
+    text-decoration: underline;
+    font-size: 1.2rem;
 `
 
 const recipe = {
@@ -83,89 +131,106 @@ const NewRecipe = () => {
             <NewRecipeCard 
                 onSubmit={handleSubmit}
             >
+                <FirstRow>
+                 
+                    <Title
+                        name="name"
+                        type="text"
+                        placeholder="Recipe name"
+                        onChange={handleChange}
+                    />
 
-                <Title
-                    name="name"
-                    type="text"
-                    placeholder="Recipe name"
-                    onChange={handleChange}
-                />
+                    <Category 
+                        name="category"
+                        onChange={handleChange}
+                    >
+                        
+                        <option value="">
+                            Select a category
+                        </option>
 
-                <Category 
-                    name="category"
-                    onChange={handleChange}
-                >
-                    
-                    <option value="">
-                        Select a category
-                    </option>
+                        <option value="breakfast">
+                            Breakfast
+                        </option>
 
-                    <option value="breakfast">
-                        Breakfast
-                    </option>
+                        <option value="lunch">
+                            Lunch
+                        </option>
 
-                    <option value="lunch">
-                        Lunch
-                    </option>
+                        <option value="dinner">
+                            Dinner
+                        </option>
 
-                    <option value="dinner">
-                        Dinner
-                    </option>
+                        <option value="snacks">
+                            Snacks
+                        </option>
 
-                    <option value="snacks">
-                        Snacks
-                    </option>
+                        <option value="drinks">
+                            Drinks
+                        </option>
 
-                    <option value="drinks">
-                        Drinks
-                    </option>
+                        <option value="dessert">
+                            Dessert
+                        </option>
 
-                    <option value="dessert">
-                        Dessert
-                    </option>
-
-                </Category>
-
+                    </Category>
+                
+                </FirstRow>
+                
                 <Description
                     name="description"
                     type="text"
                     placeholder="Describe your recipe"
-                    onChange={handleChange}
-                />
-
-                <Ingredients
-                    name="ingredients"
-                    type="text"
-                    placeholder="ingredients"
-                    onChange={handleChange}
-                />
-
-                <Instructions
-                    name="instructions"
-                    type="text"
-                    placeholder="Instructions"
                     cols="40"
-                    rows="5"
+                    rows="8"
                     onChange={handleChange}
                 />
 
-                <Text>Prep Time (minutes)</Text>
-                <TimePrep
-                    name="preptime"
-                    type="number"
-                    placeholder="0"
-                    onChange={handleChange}
-                />
+                <BottomCont>
 
-                <Text>Cook Time (minutes)</Text>
-                <TimeCook
-                    name="cooktime"
-                    type="number"
-                    placeholder="0"
-                    onChange={handleChange}
-                />
+                    <Ingredients
+                        name="ingredients"
+                        type="text"
+                        placeholder="ingredients"
+                        onChange={handleChange}
+                    />
 
-                <AddRecipe>Add recipe</AddRecipe>
+                    <RightCont>
+
+                        <Instructions
+                            name="instructions"
+                            type="text"
+                            placeholder="Instructions"
+                            cols="40"
+                            rows="5"
+                            onChange={handleChange}
+                        />
+
+                        <TimerCont>
+                            <Text>Prep Time (minutes)</Text>
+                            <Timer
+                                name="preptime"
+                                type="number"
+                                placeholder="0"
+                                onChange={handleChange}
+                            />
+                        </TimerCont>
+
+                        <TimerCont>
+                            <Text>Cook Time (minutes)</Text>
+                            <Timer
+                                name="cooktime"
+                                type="number"
+                                placeholder="0"
+                                onChange={handleChange}
+                            />
+                        </TimerCont>
+
+                        <AddRecipe>Add recipe</AddRecipe>
+
+                    </RightCont>
+                
+                </BottomCont>
 
             </NewRecipeCard>
         </Page>
