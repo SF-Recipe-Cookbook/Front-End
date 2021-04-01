364cceacd1ce3b4dde51bd0724a9d0786bb76b5f
Chad Parker <63312621+ChimpBite@users.noreply.github.com>
2021-04-01 13:01:38 -0600

    testing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom';
 
-import HomePage from './components/home-page/HomePage'
-import LoginPage from './components/log-in-page/LoginPage'
-import SignupPage from './components/sign-up-page/SignupPage'
-import ProfilePage from './components/profile-page/ProfilePage'
-import NewRecipe from './components/new-recipe-page/NewRecipe'
+import HomePage from './components/home-page/HomePage';
+import LoginPage from './components/log-in-page/LoginPagenewback';
+import SignupPage from './components/sign-up-page/SignupPage';
+import ProfilePage from './components/profile-page/ProfilePage';
+import NewRecipe from './components/new-recipe-page/NewRecipe';
 
 function App() {
   return (
     <div>
       <Switch>
-
         <Route exact path='/'>
           <HomePage />
         </Route>
@@ -30,10 +29,9 @@ function App() {
         <Route path='/new-recipe'>
           <NewRecipe />
         </Route>
-
       </Switch>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/profile-page/ProfilePage.js
+++ src/components/profile-page/ProfilePage.js
@@ -5,188 +5,207 @@ import Recipe from './RecipeComponent';
 import styled from 'styled-components';
 import 'fontsource-roboto';
 import axiosWithAuth from '../../utils/axiosWithAuth';
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 // React Component below this line
 
 const Profile = (props) => {
-    const initialState = {
-        username: '',
-        password: '',
-    }
+  const initialState = {
+    username: '',
+    password: '',
+  };
 
-    const initialRecipes = {
-        recipes: [{
-            title: ``,
-            category: '',
-            timeToMake: '',
-            description: '',
-            ingredients: '',
-            instructions: '',
-        }],
-    }
+  const initialRecipes = {
+    recipes: [
+      {
+        title: ``,
+        category: '',
+        timeToMake: '',
+        description: '',
+        ingredients: '',
+        instructions: '',
+      },
+    ],
+  };
 
-    const [user, setUser] = useState(initialState)
-    const [recipes, setRecipes] = useState(initialRecipes)
-    const [search, setSearch] = useState('')
+  console.log('Props', props);
 
-    const {push} = useHistory()
+  const [user, setUser] = useState(initialState);
+  const [recipes, setRecipes] = useState(initialRecipes);
+  const [search, setSearch] = useState('');
 
-    useEffect(() => {
-        axiosWithAuth().get('/auth')
-            .then(res => {
-                setUser(res)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-        axiosWithAuth().get('/recipes/user')
-            .then(res => {
-                setRecipes(res)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    },[])
-       
+  const { push } = useHistory();
 
+  let token = localStorage.getItem('token');
+  console.log('new token', token);
 
+  console.log('Info', localStorage);
 
-    const handleChange = e => {
-        setSearch({
-            ...search,
-            [e.target.name]: e.target.value
-        })
-    }
+  useEffect(() => {
+    axiosWithAuth()
+      .get('/auth')
+      .then((res) => {
+        console.log('res', res);
+        console.log('User', res.data);
+        console.log('Profile Id', res.data._id);
+        console.log('Profile Name', res.data.username);
+        console.log('Profile Email', res.data.email);
+        setUser(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    axiosWithAuth()
+      .get('/recipes/user')
+      .then((res) => {
+        console.log('User', res.data);
+        setRecipes(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    const handleClick = () => {
-        localStorage.removeItem('token')
-        push('/')
-      }
+  const handleChange = (e) => {
+    setSearch({
+      ...search,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const handleAdd = () => {
-        push('/new-recipe')
-    }
+  const handleClick = () => {
+    localStorage.removeItem('token');
+    push('/');
+  };
 
-    return (
-        <Page>
-            <Header>
-                <p></p>
-                <HeaderTitle>Welcome back, {user.username}</HeaderTitle>
-                <HeaderButton onClick={handleClick}>logout</HeaderButton>
-            </Header>
-            <UserCard>
-                <UserPic src="https://img.huffingtonpost.com/asset/5967ac4d2100003700fc6bc9.jpeg" alt="Person Cooking" />
-            </UserCard>
-            <RecipeSearch>
-                <p>Search for Recipe Here:</p>
-                <input
-                    type='text'
-                    name='recipe'
-                    placeholder='Recipe Name'
-                    value={search}
-                    onChange={handleChange}
-                />
-            </RecipeSearch>
-            <AddRecipeButton onClick={handleAdd}>Add a Recipe</AddRecipeButton>
-            <RecipeCards>
-                {recipes.recipes.map(recipe => {
-                    return <Recipe recipe={recipe} search={search} setRecipes={setRecipes} push={push}/>
-                })}
-            </RecipeCards>
-            <Footer>
-                <FooterButton
-                 onClick={
-                    window.scroll({
-                    top: 0, 
-                    left: 0, 
-                    behavior: 'smooth' 
-                    })}>
-                Return to Top of Page
-                </FooterButton>
-            </Footer>
-        </Page>
-    )
-}
+  const handleAdd = () => {
+    push('/new-recipe');
+  };
+
+  return (
+    <Page>
+      <Header>
+        <p></p>
+        <HeaderTitle>Welcome back, {user.username}</HeaderTitle>
+        <HeaderButton onClick={handleClick}>logout</HeaderButton>
+      </Header>
+      <UserCard>
+        <UserPic
+          src='https://img.huffingtonpost.com/asset/5967ac4d2100003700fc6bc9.jpeg'
+          alt='Person Cooking'
+        />
+      </UserCard>
+      <RecipeSearch>
+        <p>Search for Recipe Here:</p>
+        <input
+          type='text'
+          name='recipe'
+          placeholder='Recipe Name'
+          value={search}
+          onChange={handleChange}
+        />
+      </RecipeSearch>
+      <AddRecipeButton onClick={handleAdd}>Add a Recipe</AddRecipeButton>
+      {/* <RecipeCards>
+        {recipes.map((recipe) => {
+          return (
+            <Recipe
+              recipe={recipe}
+              search={search}
+              setRecipes={setRecipes}
+              push={push}
+            />
+          );
+        })}
+      </RecipeCards> */}
+      <Footer>
+        <FooterButton
+          onClick={window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+          })}
+        >
+          Return to Top of Page
+        </FooterButton>
+      </Footer>
+    </Page>
+  );
+};
 
 export default Profile;
-
 
 //styling below this line
 
 const Page = styled.div`
-    font-family: 'Roboto', sans-serif;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-`
+  font-family: 'Roboto', sans-serif;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
 
-    const Header = styled.header`
-        width: 100%;
-        height: 10vh;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
+const Header = styled.header`
+  width: 100%;
+  height: 10vh;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
 
-    `
+const HeaderTitle = styled.h1`
+  color: #813d18;
+  font-size: 40px;
+  margin-left: 105px;
+`;
 
+const HeaderButton = styled.button`
+  height: 50%;
+  width: 100px;
+  box-shadow: 3px 3px #4d6e7f;
+  margin-right: 5px;
+`;
 
-        const HeaderTitle = styled.h1`
-            color: #813D18;
-            font-size: 40px;
-            margin-left: 105px;
-        `
+const UserCard = styled.section`
+  height: 20vh;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 3px;
+`;
 
-        const HeaderButton = styled.button `
-            height: 50%;
-            width: 100px;
-            box-shadow: 3px 3px #4D6E7F;
-            margin-right: 5px;   
-        `
+const UserPic = styled.img`
+  height: 100%;
+  box-shadow: 3px 3px #4d6e7f;
+`;
 
-    const UserCard = styled.section`
-        height: 20vh;
-        width: 50%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        margin-bottom: 3px;
-    `
+const RecipeSearch = styled.form`
+  height: 10vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  margin-bottom: 5px;
+`;
+const AddRecipeButton = styled.button``;
 
-            const UserPic = styled.img`
-                height: 100%;
-                box-shadow: 3px 3px #4D6E7F;
-            `
+const RecipeCards = styled.section`
+  height: 50vh;
+  border: 2px solid black;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
 
-    const RecipeSearch = styled.form`
-         height: 10vh;
-         display: flex;
-         flex-direction: column;
-         justify-content: flex-end;
-         margin-bottom: 5px;
+const Footer = styled.footer`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+`;
 
-    `
-    const AddRecipeButton = styled.button``
-
-    const RecipeCards = styled.section`
-         height: 50vh;
-         border: 2px solid black;
-         display: flex;
-         flex-wrap: wrap;
-         flex-direction: row;
-         justify-content: center;
-         align-items: center;
-    `
-
-    const Footer = styled.footer`
-        height: 10vh;
-        display: flex;
-        justify-content: center;
-        align-items: flex-end;
-
-    `
-
-        const FooterButton = styled.button`
-        height: 50%;
-        `
+const FooterButton = styled.button`
+  height: 50%;
+`;
